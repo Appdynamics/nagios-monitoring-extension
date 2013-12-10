d1b2671f7dcca1ecb8f4e54cd123689e4b40cc90
satish.m@imaginea.com <jagwar321>
2013-12-10 19:03:20 +0530

    Code changes to make the script honour period specified (ACE-261)

--- src/main/java/com/appdynamics/monitors/nagios/NagiosMonitor.java
+++ src/main/java/com/appdynamics/monitors/nagios/NagiosMonitor.java
@@ -1,83 +1,74 @@
 
 package com.appdynamics.monitors.nagios;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.log4j.Logger;
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Element;
-import org.dom4j.io.SAXReader;
-
 import com.appdynamics.monitors.nagios.script.NagiosScript;
 import com.singularity.ee.agent.systemagent.api.AManagedMonitor;
 import com.singularity.ee.agent.systemagent.api.MetricWriter;
 import com.singularity.ee.agent.systemagent.api.TaskExecutionContext;
 import com.singularity.ee.agent.systemagent.api.TaskOutput;
 import com.singularity.ee.agent.systemagent.api.exception.TaskExecutionException;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.apache.log4j.Logger;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
 /**
  * @author
  */
-public class NagiosMonitor extends AManagedMonitor
-{
+public class NagiosMonitor extends AManagedMonitor {
     private static final String METRICS_PREFIX = "Custom Metrics|Monitoring|Nagios|Status|"; // Controller Path.
     private static final Logger LOG = Logger.getLogger(NagiosMonitor.class); // The Logger.
     private static final int WORKER_COUNT = 10; // Max worker count can be altered via configuration.
-    private static final int MAX_WAIT_TIME = 5; // Max wait time in minutes for the workers to go down.
 
-    private String projectFolder; // The prokect folder.
+    private String projectFolder; // The project folder.
     private Integer workerCount = WORKER_COUNT;
-    private Integer maxWaitTime = MAX_WAIT_TIME;
 
     private Collection<NagiosScript> scripts; // Collection of tasks (implementing java.lang.Runnable)
-    private final HashMap<String, String> results = new HashMap<String, String>(); // The results map.
 
     /**
      * This represents the various valid NAGIOS STATUS.
-     * 
-     * @author 
+     *
+     * @author
      */
-    private enum STATES
-    {
+    private enum STATES {
         OK, WARNING, CRITICAL, UNKNOWN; // States
 
         /**
          * @see java.lang.Enum#toString()
          */
         @Override
-        public String toString()
-        {
+        public String toString() {
             return String.valueOf(this.ordinal());
         }
     }
 
     /**
-     * 
+     *
      */
-    public void print()
-    {
+    public void print() {
         LOG.info("Printing metrics.");
 
         // Publish status to the controller instance.
         for (NagiosScript script : scripts) {
             if (!script.isRunAll()) {
-                LOG.error("NAME: " + script.getName() + " VALUE: " + results.get(script.getId()));
+                String exitCode = String.valueOf(script.getExitCode());
+                LOG.error("NAME: " + script.getName() + " VALUE: " + exitCode);
                 printMetric(
-                    script.getName(),
-                    (results.get(script.getId()) == null) ? STATES.UNKNOWN : results.get(script.getId()),
-                    MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION, MetricWriter.METRIC_TIME_ROLLUP_TYPE_CURRENT,
-                    MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE);
+                        script.getName(),
+                        exitCode,
+                        MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION, MetricWriter.METRIC_TIME_ROLLUP_TYPE_CURRENT,
+                        MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE);
             }
         }
     }
@@ -86,16 +77,15 @@ public class NagiosMonitor extends AManagedMonitor
      * @param xml
      * @throws DocumentException
      */
-    public void parseXML(final String xml) throws DocumentException
-    {
+    public void parseXML(final String xml) throws DocumentException {
         scripts = new LinkedList<NagiosScript>();
 
         final SAXReader reader = new SAXReader();
         final Document document = reader.read(xml);
         final Element root = document.getRootElement();
 
-        for (Iterator<Element> i = root.elementIterator(); i.hasNext();) {
-            Element element = (Element) i.next();
+        for (Iterator<Element> i = root.elementIterator(); i.hasNext(); ) {
+            Element element = i.next();
 
             if (element.getName().equals("script")) {
                 Iterator<Element> elementIterator = element.elementIterator();
@@ -103,38 +93,37 @@ public class NagiosMonitor extends AManagedMonitor
                 if (element.attributeCount() > 0 && element.attribute(0).getName().equals("run-all")) {
                     String path = null;
                     int period = -1;
-                    for (Iterator<Element> j = elementIterator; j.hasNext();) {
-                        element = (Element) j.next();
+                    int maxWaitTime = -1;
+                    for (Iterator<Element> j = elementIterator; j.hasNext(); ) {
+                        element = j.next();
                         if (element.getName().equals("path")) {
                             path = element.getText();
-                        }
-                        else if (element.getName().equals("period")) {
+                        } else if (element.getName().equals("period")) {
                             period = Integer.parseInt(element.getText());
+                        } else if (element.getName().equals("max-wait-time")) {
+                            maxWaitTime = Integer.parseInt(element.getText());
                         }
                     }
 
                     try {
-                        iterateFolder(new File(path), period);
-                    }
-                    catch (IOException e) {
+                        iterateFolder(new File(path), period, maxWaitTime);
+                    } catch (IOException e) {
                         LOG.error("Failed to gather scripts from the path given: " + path, e);
                     }
-                }
-                else {
+                } else {
                     final NagiosScript script = new NagiosScript();
-                    for (Iterator<Element> j = elementIterator; j.hasNext();) {
-                        element = (Element) j.next();
+                    for (Iterator<Element> j = elementIterator; j.hasNext(); ) {
+                        element = j.next();
                         if (element.getName().equals("name")) {
                             script.setName(element.getText());
-                        }
-                        else if (element.getName().equals("path")) {
+                        } else if (element.getName().equals("path")) {
                             script.setPath(element.getText());
-                        }
-                        else if (element.getName().equals("period")) {
+                        } else if (element.getName().equals("period")) {
                             script.setPeriod(Integer.parseInt(element.getText()));
-                        }
-                        else if (element.getName().equals("script-arguments")) {
+                        } else if (element.getName().equals("script-arguments")) {
                             script.setArguments(element.getText());
+                        } else if (element.getName().equals("max-wait-time")) {
+                            script.setMaxWaitTime(Integer.parseInt(element.getText()));
                         }
                     }
 
@@ -150,18 +139,17 @@ public class NagiosMonitor extends AManagedMonitor
      * @param period
      * @throws IOException
      */
-    public void iterateFolder(final File folder, final Integer period) throws IOException
-    {
+    public void iterateFolder(final File folder, final Integer period, final Integer maxWaitTime) throws IOException {
         for (final File file : folder.listFiles()) {
             if (file.isDirectory()) {
-                iterateFolder(file, period);
-            }
-            else if (!file.isHidden() && !file.getName().startsWith(".") && file.getName() != null) {
+                iterateFolder(file, period, maxWaitTime);
+            } else if (!file.isHidden() && !file.getName().startsWith(".") && file.getName() != null) {
                 final NagiosScript script = new NagiosScript();
                 script.setId(UUID.randomUUID().toString());
                 script.setName(file.getName());
                 script.setPath(file.getAbsolutePath());
                 script.setPeriod(period);
+                script.setMaxWaitTime(maxWaitTime);
                 scripts.add(script);
             }
         }
@@ -169,29 +157,28 @@ public class NagiosMonitor extends AManagedMonitor
 
     /**
      * Returns the metric to the AppDynamics Controller.
-     * @param 	metricName		Name of the Metric
-     * @param 	metricValue		Value of the Metric
-     * @param 	aggregation		Average OR Observation OR Sum
-     * @param 	timeRollup		Average OR Current OR Sum
-     * @param 	cluster			Collective OR Individual
+     *
+     * @param metricName  Name of the Metric
+     * @param metricValue Value of the Metric
+     * @param aggregation Average OR Observation OR Sum
+     * @param timeRollup  Average OR Current OR Sum
+     * @param cluster     Collective OR Individual
      */
     public void printMetric(
-        final String metricName,
-        final Object metricValue,
-        final String aggregation,
-        final String timeRollup,
-        final String cluster)
-    {
+            final String metricName,
+            final Object metricValue,
+            final String aggregation,
+            final String timeRollup,
+            final String cluster) {
         final MetricWriter metricWriter = getMetricWriter(
-            getMetricPrefix() + metricName, aggregation, timeRollup, cluster);
+                getMetricPrefix() + metricName, aggregation, timeRollup, cluster);
         metricWriter.printMetric(String.valueOf(metricValue));
     }
 
     /**
      * @return
      */
-    protected String getMetricPrefix()
-    {
+    protected String getMetricPrefix() {
         return METRICS_PREFIX;
     }
 
@@ -199,82 +186,56 @@ public class NagiosMonitor extends AManagedMonitor
      * @see com.singularity.ee.agent.systemagent.api.ITask#execute(java.util.Map, com.singularity.ee.agent.systemagent.api.TaskExecutionContext)
      */
     public TaskOutput execute(final Map<String, String> args, final TaskExecutionContext context)
-        throws TaskExecutionException
-    {
+            throws TaskExecutionException {
         parseArgs(args);
 
         // Parse the scripts.xml.
         try {
             parseXML(projectFolder + "/conf/scripts.xml");
-        }
-        catch (DocumentException e) {
+        } catch (DocumentException e) {
             LOG.error("Failed to parse XML." + e.toString());
         }
 
-        for (;;) {
-            // Initiate the pool with the pre defined work count.
-            final ExecutorService executor = Executors.newFixedThreadPool(this.workerCount);
+        final ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(this.workerCount);
 
-            // Add the tasks to execute them in some time in future.
-            for (final NagiosScript script : scripts) {
-                executor.execute(script);
-            }
-
-            // Ask the executor to gracefully shutdown after every thing is finished.
-            executor.shutdown();
-
-            // Now see that this pool shutsdown and does not go to eternity.
-            try {
-                executor.awaitTermination(this.maxWaitTime * 60, TimeUnit.SECONDS);
-            }
-            catch (InterruptedException e) {
-                LOG.error("Failed to shutdown the executor in max wait time(in minutes):" + MAX_WAIT_TIME, e);
-            }
-
-            // After every thing has been done gracefully,
-            // check the status of nagios script execution and send it to controller.
-            // Gather status.
-            for (final NagiosScript script : scripts) {
-                this.results.put(script.getId(), String.valueOf(script.getExitCode()));
-
-                if (script.getExitCode() > 2) {
-                    LOG.warn("Unknown exit code for script: " + script.getName());
-                }
-            }
-
-            // Send them across the controller.
-            print();
-
-            LOG.info("Scripts executed.");
+        // Add the tasks to execute them in some time in future.
+        for (final NagiosScript script : scripts) {
+            //Submit the periodic task according to period specified.
+            scheduledThreadPoolExecutor.scheduleAtFixedRate(script, 0, script.getPeriod(), TimeUnit.SECONDS);
         }
+
+        // Send the result across the controller.
+        final ScheduledThreadPoolExecutor outputScheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(1);
+        outputScheduledThreadPoolExecutor.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                print();
+            }
+        }, 0, 60, TimeUnit.SECONDS);
+
+        //As this is continuous extension, make this thread wait indefinitely.
+        CountDownLatch infiniteWait = new CountDownLatch(1);
+        try {
+            infiniteWait.await();   //Will make this thread to wait till the CountDownLatch reaches to 0.
+        } catch (InterruptedException e) {
+            LOG.error("Failed to wait indefinitely ", e);
+        }
+
+        return new TaskOutput("Finished executing Nagios Monitor");
     }
 
     /**
      * @param args
      */
-    public void parseArgs(final Map<String, String> args)
-    {
+    public void parseArgs(final Map<String, String> args) {
         // Get the path.
         this.projectFolder = args.get("project_path");
 
-        // worker_count, max_wait_time
-        final String strMaxWaitTime = args.get("max_wait_time");
         final String strWorkerCount = args.get("worker_count");
-
-        if (isNotEmpty(strMaxWaitTime)) {
-            try {
-                this.maxWaitTime = Integer.parseInt(strMaxWaitTime);
-            }
-            catch (IllegalArgumentException e) {
-                // Ignore.
-            }
-        }
 
         if (isNotEmpty(strWorkerCount)) {
             try {
                 this.workerCount = Integer.parseInt(strWorkerCount);
-            }
-            catch (IllegalArgumentException e) {
+            } catch (IllegalArgumentException e) {
                 // Ignore.
             }
         }
@@ -284,8 +245,7 @@ public class NagiosMonitor extends AManagedMonitor
      * @param input
      * @return
      */
-    public static boolean isNotEmpty(final String input)
-    {
+    public static boolean isNotEmpty(final String input) {
         return null != input && !"".equals(input.trim());
     }
 
--- src/main/java/com/appdynamics/monitors/nagios/script/NagiosScript.java
+++ src/main/java/com/appdynamics/monitors/nagios/script/NagiosScript.java
@@ -1,18 +1,15 @@
-
 package com.appdynamics.monitors.nagios.script;
 
-import java.io.IOException;
-
-import org.apache.log4j.Logger;
-
 import com.appdynamics.monitors.nagios.NagiosMonitor;
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import org.apache.log4j.Logger;
 
 /**
  * @author
- *
  */
-public class NagiosScript implements Runnable
-{
+public class NagiosScript implements Runnable {
     public static Logger LOG = Logger.getLogger(NagiosMonitor.class);
 
     private String id;
@@ -25,6 +22,7 @@ public class NagiosScript implements Runnable
     private boolean runAll = false;
     private boolean forceStop = true;
     private Integer exitCode = 3; // UNKNOWN for Nagios.
+    private Integer maxWaitTime;
 
     /**
      * @param id
@@ -34,21 +32,12 @@ public class NagiosScript implements Runnable
      * @param arguments
      * @param periodPassed
      * @param started
-     * @param thread
      * @param runAll
      * @param forceStop
+     * @param maxWaitTime
      */
-    public NagiosScript(
-        final String id,
-        final String name,
-        final String path,
-        final Integer period,
-        final String arguments,
-        final Integer periodPassed,
-        final boolean started,
-        final boolean runAll,
-        final boolean forceStop)
-    {
+    public NagiosScript(final String id, final String name, final String path, final Integer period, final String arguments, final Integer periodPassed, final boolean started,
+                        final boolean runAll, final boolean forceStop, final Integer maxWaitTime) {
         this.id = id;
         this.name = name;
         this.path = path;
@@ -58,189 +47,243 @@ public class NagiosScript implements Runnable
         this.started = started;
         this.runAll = runAll;
         this.forceStop = forceStop;
+        this.maxWaitTime = maxWaitTime;
     }
 
-    public NagiosScript()
-    {
+    public NagiosScript() {
         super();
     }
 
     /**
      * @see java.lang.Runnable#run()
      */
-    public void run()
-    {
+    public void run() {
+        Runtime runtime = Runtime.getRuntime();
+        Process process = null;
         try {
-            this.exitCode = Runtime.getRuntime().exec(this.path + " " + this.arguments).waitFor();
-        }
-        catch (IOException e) {
-            LOG.error("Unable to run script: " + this.name, e);
-        }
-        catch (InterruptedException e) {
+            process = runtime.exec(this.path + " " + this.arguments);
+        } catch (IOException e) {
             LOG.error("Unable to run script: " + this.name, e);
         }
 
-        LOG.info("Exit code for script: " + this.name + " : " + this.exitCode);
+        CountDownLatch doneSignal = new CountDownLatch(1);
+        Worker worker = new Worker(this.name, process, doneSignal);
+        worker.start();
+        try {
+            doneSignal.await(this.maxWaitTime, TimeUnit.SECONDS);
+
+            if (worker.getExitCode() != null) {
+
+                if (worker.getExitCode() > 3) { //Nagios status codes are 0, 1, 2, 3
+                    this.exitCode = 3;
+                } else {
+                    this.exitCode = worker.getExitCode();
+                }
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Exit code for script : " + this.name + " is :" + this.exitCode);
+                }
+            } else {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("No Exit code for script : " + this.name);
+                }
+            }
+        } catch (InterruptedException e) {
+            worker.interrupt();
+            Thread.currentThread().interrupt();
+            LOG.error("Unable to run script: " + this.name, e);
+        } finally {
+            if (process != null) {
+                process.destroy();
+            }
+        }
     }
 
     /**
      * @return
      */
-    public String getId()
-    {
+    public String getId() {
         return id;
     }
 
     /**
      * @param id
      */
-    public void setId(final String id)
-    {
+    public void setId(final String id) {
         this.id = id;
     }
 
     /**
      * @return
      */
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
     /**
      * @param name
      */
-    public void setName(final String name)
-    {
+    public void setName(final String name) {
         this.name = name;
     }
 
     /**
      * @return
      */
-    public String getPath()
-    {
+    public String getPath() {
         return path;
     }
 
     /**
      * @param path
      */
-    public void setPath(final String path)
-    {
+    public void setPath(final String path) {
         this.path = path;
     }
 
     /**
      * @return
      */
-    public Integer getPeriod()
-    {
+    public Integer getPeriod() {
         return period;
     }
 
     /**
      * @param period
      */
-    public void setPeriod(final Integer period)
-    {
+    public void setPeriod(final Integer period) {
         this.period = period;
     }
 
     /**
      * @return
      */
-    public String getArguments()
-    {
+    public String getArguments() {
         return arguments;
     }
 
     /**
      * @param arguments
      */
-    public void setArguments(final String arguments)
-    {
+    public void setArguments(final String arguments) {
         this.arguments = arguments;
     }
 
     /**
      * @return
      */
-    public Integer getPeriodPassed()
-    {
+    public Integer getPeriodPassed() {
         return periodPassed;
     }
 
     /**
      * @param periodPassed
      */
-    public void setPeriodPassed(final Integer periodPassed)
-    {
+    public void setPeriodPassed(final Integer periodPassed) {
         this.periodPassed = periodPassed;
     }
 
     /**
      * @return
      */
-    public boolean isStarted()
-    {
+    public boolean isStarted() {
         return started;
     }
 
     /**
      * @param started
      */
-    public void setStarted(final boolean started)
-    {
+    public void setStarted(final boolean started) {
         this.started = started;
     }
 
     /**
      * @return
      */
-    public boolean isRunAll()
-    {
+    public boolean isRunAll() {
         return runAll;
     }
 
     /**
      * @param runAll
      */
-    public void setRunAll(final boolean runAll)
-    {
+    public void setRunAll(final boolean runAll) {
         this.runAll = runAll;
     }
 
     /**
      * @return
      */
-    public boolean isForceStop()
-    {
+    public boolean isForceStop() {
         return forceStop;
     }
 
     /**
      * @param forceStop
      */
-    public void setForceStop(final boolean forceStop)
-    {
+    public void setForceStop(final boolean forceStop) {
         this.forceStop = forceStop;
     }
 
     /**
      * @return
      */
-    public Integer getExitCode()
-    {
+    public Integer getExitCode() {
         return exitCode;
     }
 
     /**
      * @param exitCode
      */
-    public void setExitCode(final Integer exitCode)
-    {
+    public void setExitCode(final Integer exitCode) {
         this.exitCode = exitCode;
     }
 
+    /**
+     * @return
+     */
+    public Integer getMaxWaitTime() {
+        return maxWaitTime;
+    }
+
+    /**
+     * @param maxWaitTime
+     */
+    public void setMaxWaitTime(Integer maxWaitTime) {
+        if(maxWaitTime <= 0) {
+            maxWaitTime = 5;  //If not specified, wait for 5 seconds
+        }
+        this.maxWaitTime = maxWaitTime;
+    }
+
+    class Worker extends Thread {
+        private final Process process;
+        private Integer exitCode;
+        private final CountDownLatch countDownLatch;
+        private final String scriptName;
+
+        Worker(String scriptName, Process process, CountDownLatch countDownLatch) {
+            this.process = process;
+            this.countDownLatch = countDownLatch;
+            this.scriptName = scriptName;
+        }
+
+        public void run() {
+            try {
+                exitCode = process.waitFor();
+            } catch (InterruptedException ignore) {
+                // NOOP
+            } finally {
+                countDownLatch.countDown();
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Exit code from worker for script : " + this.scriptName + " is :" + this.exitCode);
+                }
+            }
+        }
+
+        public Integer getExitCode() {
+            return exitCode;
+        }
+    }
 }
+
+
